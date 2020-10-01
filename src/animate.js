b190b44a4310847901bf949899100b6235a7d91a
Kaue Pavillion LINUX <kaue.sedrez@gmail.com>
2020-10-01 15:27:38 -0300

    1.0.6 - queryselectorall

--- src/animate.js
+++ src/animate.js
@@ -1,34 +1,35 @@
+import { Winnetou } from "../../winnetoujs/src/winnetou.js";
+
 /**
  * Create css animations
  * Credits to Copyright (c) 2020 Daniel Eden from https://animate.style
  * @param  {string} element tag("#myDiv"), class(".selectors") or tag("p")
  * @param  {string} animation animation name
- * @param  {string=} speed default **fast**
+ * @param  {"fast"|"faster"|"slower"|"slow"=} speed default **fast**
+ *
+ * Todo:
+ * --> jsdoc speed params
+ * --> query selector all
  */
 export function anim(element, animation, speed = "fast") {
   return new Promise((resolve, reject) => {
     const prefix = "animate__";
     const animationName = `${prefix}${animation}`;
-    var node = document.querySelector(element);
+    const node = Winnetou.select(element).getEl(element);
 
-    if (!node) {
-      node = document.getElementById(element);
-    }
-
-    node.classList.add(
-      `${prefix}animated`,
-      `${prefix}${speed}`,
-      animationName
-    );
+    Winnetou.select(element)
+      .addClass(`${prefix}animated`)
+      .addClass(`${prefix}${speed}`)
+      .addClass(animationName);
 
     // When the animation ends, we clean the classes and resolve the Promise
     function handleAnimationEnd() {
-      node.classList.remove(`${prefix}animated`, animationName);
-      node.removeEventListener("animationend", handleAnimationEnd);
+      node[0].classList.remove(`${prefix}animated`, animationName);
+      node[0].removeEventListener("animationend", handleAnimationEnd);
 
       resolve("Animation ended");
     }
 
-    node.addEventListener("animationend", handleAnimationEnd);
+    node[0].addEventListener("animationend", handleAnimationEnd);
   });
 }
